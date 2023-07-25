ceb7d24eba431dd03c2b2be70d08b776ab2739c7
Sakar <infosakardangol123@gmail.com>
2023-07-25 16:06:13 +0545

    Post Save to DB added

--- Areas/Admin/Controllers/PostController.cs
+++ Areas/Admin/Controllers/PostController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using OurSunday.Data;
+using OurSunday.Models;
 using OurSunday.ViewModel;
+using System.Runtime.InteropServices;
 
 namespace OurSunday.Areas.Admin.Controllers
 {
@@ -7,6 +14,22 @@ namespace OurSunday.Areas.Admin.Controllers
 
     public class PostController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _WebHostenvironment;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public INotyfService _notification { get; } 
+
+        public PostController(ApplicationDbContext context, INotyfService notyfService, IWebHostEnvironment webHostenvironment,UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _notification = notyfService;
+            _WebHostenvironment = webHostenvironment;
+            _userManager = userManager;
+        }
+
+
+
         public IActionResult Index()
         {
             return View();
@@ -15,7 +38,60 @@ namespace OurSunday.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult Create()
         {
+
             return View(new CreatePostVM());
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePostVM vm)
+        {
+            if (!ModelState.IsValid) { return View(vm); }
+
+            //get login id 
+
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
+
+            var post = new Post();
+
+            post.Title = vm.Title; 
+            post.Description = vm.Description;
+            post.ShortDescription = vm.ShortDescription;
+            post.ApplicationUserid = loggedInUser!.Id;
+
+            if(post.Title!=null)
+            {
+                string slug = vm.Title!.Trim();
+                slug = slug.Replace(" ", "-");
+                post.Slug = slug + "-" + Guid.NewGuid();
+            }
+
+
+            if (vm.Thumbnail != null)
+            {
+                post.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+
+            await _context.Posts!.AddAsync(post);
+            await _context.SaveChangesAsync();
+            _notification.Success("Post Published Succesfully.");
+            return RedirectToAction("Index");
+
+
+        }
+
+
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = "";
+            var folderpath = Path.Combine(_WebHostenvironment.WebRootPath, "thumbnails");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filePath= Path.Combine(folderpath, uniqueFileName);
+            using(FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fileStream);
+            }
+            return uniqueFileName;
+        }
+
     }
 }
--- ViewModel/CreatePostVM.cs
+++ ViewModel/CreatePostVM.cs
@@ -1,11 +1,14 @@
-﻿using OurSunday.Models;
+﻿using Microsoft.Build.Framework;
+using OurSunday.Models;
 
 namespace OurSunday.ViewModel
 {
     public class CreatePostVM
     {
         public int Id { get; set; }
+        [Required]
         public string? Title { get; set; }
+        
         public string? ShortDescription { get; set; }
         public string? ApplicationUserid { get; set; }
         public string? Description { get; set; }
