939523733c9a9f6c0a02d46ea6b21d06adc04c56
Sakar <infosakardangol123@gmail.com>
2023-07-25 15:07:31 +0545

    UserPostPage Add

--- Areas/Admin/Controllers/PostController.cs
+++ Areas/Admin/Controllers/PostController.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OurSunday.ViewModel;
 
 namespace OurSunday.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+
     public class PostController : Controller
     {
-        [Area("Admin")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreatePostVM());
         }
     }
 }
